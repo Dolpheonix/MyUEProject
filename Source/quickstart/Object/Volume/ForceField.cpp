aee1cbeec33069744b335f41d95c166e69849446
Dolphoenix <moonfog97@gmail.com>
2023-02-04 01:40:37 +0900

    230204

--- Source/quickstart/Object/Volume/ForceField.cpp
+++ Source/quickstart/Object/Volume/ForceField.cpp
@@ -3,21 +3,60 @@
 
 #include "ForceField.h"
 #include "Kismet/GameplayStatics.h"
+#include "GameFramework/Character.h"
+#include "GameFramework/CharacterMovementComponent.h"
+#include "../../Utils/Helpers.h"
 #include "Components/BrushComponent.h"
 
 AForceField::AForceField()
 {
+	PrimaryActorTick.bCanEverTick = true;
 	GetBrushComponent()->SetCollisionProfileName("Volume");
+
+	Arrow = CreateDefaultSubobject<UArrowComponent>(TEXT("Arrow"));
+	Helpers::SetComponent(&Arrow, GetBrushComponent(), FVector::ZeroVector, FRotator(90.0f, 0.0f, 0.0f));
+}
+
+void AForceField::BeginPlay()
+{
+	Super::BeginPlay();
+	ForceDirection = Arrow->GetComponentRotation().Vector();
 }
 
 void AForceField::ActorEnteredVolume(AActor* other)
 {
 	Super::ActorEnteredVolume(other);
 	GEngine->AddOnScreenDebugMessage(-1, 5, FColor::Blue, TEXT("ENTER??"));
+	auto player = UGameplayStatics::GetPlayerCharacter(this, 0);
+	if ((AActor*)player == other)
+	{
+		isActivate = true;
+	}
 }
 
 void AForceField::ActorLeavingVolume(AActor* other)
 {
 	Super::ActorLeavingVolume(other);
 	GEngine->AddOnScreenDebugMessage(-1, 5, FColor::Red, TEXT("LEAVE??"));
+
+	auto player = UGameplayStatics::GetPlayerCharacter(this, 0);
+	if ((AActor*)player == other)
+	{
+		player->GetCharacterMovement()->GravityScale = 1.0f;
+		isActivate = false;
+	}
+}
+
+void AForceField::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+	auto player = Cast<ACharacter>(UGameplayStatics::GetPlayerPawn(this, 0));
+	if (isActivate)
+	{
+		float mass = player->GetCharacterMovement()->Mass;
+		FVector Force = ForceDirection * 10000.0f * mass * ForceMagnitude;
+		player->GetCharacterMovement()->GravityScale = 0.0f;
+		player->GetCharacterMovement()->AddForce(Force);
+	}
 }
--- Source/quickstart/Object/Volume/ForceField.h
+++ Source/quickstart/Object/Volume/ForceField.h
@@ -4,12 +4,13 @@
 
 #include "CoreMinimal.h"
 #include "GameFramework/PhysicsVolume.h"
+#include "Components/ArrowComponent.h"
 #include "ForceField.generated.h"
 
 /**
  * 
  */
-UCLASS()
+UCLASS(hidecategories = (CharacterMovement))
 class QUICKSTART_API AForceField : public APhysicsVolume
 {
 	GENERATED_BODY()
@@ -20,5 +21,19 @@ public:
 	virtual void ActorEnteredVolume(class AActor* Other) override;
 
 	virtual void ActorLeavingVolume(class AActor* Other) override;
+
+	virtual void BeginPlay() override;
+
+	virtual void Tick(float DeltaTime) override;
 	
+public:
+	bool isActivate = false;
+
+	UPROPERTY(VisibleAnywhere)
+	UArrowComponent* Arrow;
+
+	FVector ForceDirection;
+
+	UPROPERTY(EditAnywhere, Category = "Force")
+	float ForceMagnitude = 1.0f;
 };
